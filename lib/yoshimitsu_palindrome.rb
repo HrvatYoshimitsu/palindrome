2a6b57feea4dcc5d79313b7275ed9135d4bd1389
Josip Dorvak <[jdorvak@gmail.com]>
2020-07-02 20:29:09 -0400

    finish working and refactoring palindrome method

--- lib/yoshimitsu_palindrome.rb
+++ lib/yoshimitsu_palindrome.rb
@@ -1,6 +1,12 @@
 require "yoshimitsu_palindrome/version"
 
-module YoshimitsuPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+  private
+
+    def processed_content
+      self.scan(/[a-z]/i).join.downcase
+    end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,4 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "yoshimitsu_palindrome"
 require "minitest/autorun"
 require "minitest/reporters"
-Minitest::Reporters.use!
+MiniTest::Reporters.use!
